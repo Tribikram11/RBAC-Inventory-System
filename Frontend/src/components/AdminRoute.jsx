2af9d688d6c799c9fba81e52c96709056ee33809
Tribikram11 <22btaml33@suiit.ac.in>
2025-12-20 23:54:06 +0530

    Initial commit

---
+++ Frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+
+function AdminRoute() {
+    
+    const { user, isAuthenticated } = useAuth();
+   
+
+    if(!isAuthenticated){
+        return <Navigate to="/login" replace/>
+        
+    }
+
+    if(user.role !== "admin"){
+        return <Navigate to="/" replace/>
+    }
+
+    return <Outlet/>
+
+}
+
+export default AdminRoute;
